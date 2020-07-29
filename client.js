8502f875af8abb2e6ec4f928ce6fb93c9e0949b5
Christy Lum <christy.lum121@gmail.com>
2020-07-29 22:55:29 +0000

    Updated files to get snake to move

---
+++ client.js
@@ -0,0 +1,35 @@
+//Establishes connection with the game server
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+  });
+  
+  //Interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('connection successful');
+    conn.write('Name: CEL');
+    
+    setInterval(() => {
+      conn.write("Move: up");
+    },500);
+  })
+
+  return conn;
+}
+
+module.exports = connect;
+
+
+/*
+
+Movement commands supported by game server: 
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)
+
+*/
--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require('net');
-
-//Establishes connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50541
-  });
-
-  //Interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('connect', () => {
-    console.log('connection successful');
-  })
-
-  return conn;
-}
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
