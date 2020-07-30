a97f2293fa42df9e134659183e5e09518cde7450
Christy Lum <christy.lum121@gmail.com>
2020-07-30 01:26:47 +0000

    removed functions from play.js

--- play.js
+++ play.js
@@ -1,46 +1,5 @@
 const connect = require('./client');
-
-const setupInput = function(connection) {
-  const stdin = process.stdin;
-  
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  
-  stdin.on('data', function(key) {
-    handleUserInput(key, connection);
-  });
-  return stdin;
-}
+const setupInput = require('./input');
 
 console.log('Connecting ...');
 connect(setupInput);
-
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-
-const handleUserInput = function(key, conn) {
-    //WASD
-    if (key === 'w') {
-      conn.write("Move: up");
-    }
-  
-    if (key === 'a') {
-      conn.write("Move: left");
-    }
-
-    if (key === 's') {
-      conn.write("Move: down");
-    }
-
-    if (key === 'd') {
-      conn.write("Move: right");
-    }
-
-    if (key === '\u0003') {
-      process.exit();
-    }
-}
-
